12d2ded925d458407d9b0b758c3069534973055c
DIM云龙 <hello@z2z.cn>
2024-05-09 19:31:02 +0800

    修改路由为hash模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import index from '@/views/Index/index.vue'
+import User from '@/views/User/index.vue'
+import Content from '@/views/Content/index.vue'
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory('/'),
   routes: [
     {
       path: '/',
@@ -10,19 +13,19 @@ const router = createRouter({
       children: [
         {
           path: '/user',
-          component: () => import('@/views/User/index.vue'),
+          component: User,
           meta: { title: '用户中心' }
         },
         {
           path: '/',
-          component: () => import('@/views/Content/index.vue'),
+          component: Content,
           meta: { title: '首页' }
         }
       ]
     }
   ]
 })
-router.afterEach((to) => {
+router.afterEach((to, next) => {
   // 获取目标路由的meta中的title信息 进行拼接，修改 页面title
   document.title = `${to.meta.title || ''} - Me And GEM `
   const contentContainer = document.querySelector('.main')
@@ -32,5 +35,12 @@ router.afterEach((to) => {
     // 如果没有找到特定容器，则尝试滚动整个窗口
     window.scrollTo(0, 0)
   }
+  if (to.matched.length === 0) {
+    // 重定向到默认路由
+    next('/')
+  } else {
+    next()
+  }
+  next()
 })
 export default router
